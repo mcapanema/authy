aa6711613fd4faa5b4f6bace53d6e3e040b38e41
Murilo Capanema <murilo.capanema@gmail.com>
2017-02-28 22:47:13 -0300

    Initial setup for firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBN45kmckpenOQrX-iMUQC73ybPCqpFDOo',
+      authDomain: 'auth-e7a59.firebaseapp.com',
+      databaseURL: 'https://auth-e7a59.firebaseio.com',
+      storageBucket: 'auth-e7a59.appspot.com',
+      messagingSenderId: '11190766205'
+    });
+  }
+
   render() {
     return (
       <View>
